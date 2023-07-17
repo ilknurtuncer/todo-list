3da28451fb1fbc25546079f49747bb19209a503a
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-17 16:46:36 +0300

    todoınput and todoitem add

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// const Header = ({ addTodo }) => {
+//   const [inputValue, setInputValue] = useState("");
+
+
+  return <div>
+  <h1 className="text-3xl font-bold mb-4">To-Do List</h1>
+<div>
+    {/* <input
+    type="text"
+    value={inputValue}
+    onChange={handleInputChange}
+    className="border border-gray-300 rounded-l px-4 py-2 w-full"
+    /> */}
+  
+</div>
+
+
+  </div>;
+};
+
+export default Header;
